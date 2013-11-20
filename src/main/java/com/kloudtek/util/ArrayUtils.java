84ad4a701821cb75270ca8999211674ce0b109df
Yannick Menager <ymenager@kloudtek.com>
2013-11-20 22:08:58 +0000

    ArrayUtils improvements

--- src/main/java/com/kloudtek/util/ArrayUtils.java
+++ src/main/java/com/kloudtek/util/ArrayUtils.java
@@ -4,60 +4,69 @@
 
 package com.kloudtek.util;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by yannick on 13/11/13.
  */
 public class ArrayUtils {
-    public static boolean[] concat(boolean[] array1, boolean[] array2) {
+    public static <X extends Object> X[] concat(X[] array1, X... array2) {
+        X[] result = (X[]) Array.newInstance(array1.getClass().getComponentType(), array1.length + array2.length);
+        System.arraycopy(array1, 0, result, 0, array1.length);
+        System.arraycopy(array2, 0, result, array1.length, array2.length);
+        return result;
+    }
+
+    public static boolean[] concat(boolean[] array1, boolean... array2) {
         boolean[] result = new boolean[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static byte[] concat(byte[] array1, byte[] array2) {
+    public static byte[] concat(byte[] array1, byte... array2) {
         byte[] result = new byte[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static short[] concat(short[] array1, short[] array2) {
+    public static short[] concat(short[] array1, short... array2) {
         short[] result = new short[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static int[] concat(int[] array1, int[] array2) {
+    public static int[] concat(int[] array1, int... array2) {
         int[] result = new int[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static long[] concat(long[] array1, long[] array2) {
+    public static long[] concat(long[] array1, long... array2) {
         long[] result = new long[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static float[] concat(float[] array1, float[] array2) {
+    public static float[] concat(float[] array1, float... array2) {
         float[] result = new float[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static double[] concat(double[] array1, double[] array2) {
+    public static double[] concat(double[] array1, double... array2) {
         double[] result = new double[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
 
-    public static char[] concat(char[] array1, char[] array2) {
+    public static char[] concat(char[] array1, char... array2) {
         char[] result = new char[array1.length + array2.length];
         System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
