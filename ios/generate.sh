d7439c65e98c5a05e0d77128f956d707903dbe89
Yannick Menager <ymenager@kloudtek.com>
2014-01-10 07:54:54 +0000

    Updated IOS Code

--- ios/generate.sh
+++ ios/generate.sh
@@ -6,10 +6,10 @@ rsync -a java/ _java
 rsync -a ../src/main/java/ _java
 KU=com/kloudtek/util
 
-j2objc -use-arc --no-package-directories --prefix com.kloudtek.util=KTUtils --prefix com.kloudtek.util.io=KTUtils \
+j2objc -use-arc --no-package-directories --prefix com.kloudtek.util=KTUtil --prefix com.kloudtek.util.io=KTUtil \
     -sourcepath _java -d KTUtils java/net/URLDecoder.java ${KU}/StringUtils.java ${KU}/BaseNCodec.java ${KU}/Base64.java ${KU}/Base32.java \
     ${KU}/UnexpectedException.java ${KU}/io/IOUtils.java ${KU}/io/DataInputStream.java ${KU}/io/DataOutputStream.java  \
-    ${KU}/io/ByteArrayDataInputStream.java ${KU}/io/ByteArrayDataOutputStream.java
+    ${KU}/io/ByteArrayDataInputStream.java ${KU}/io/ByteArrayDataOutputStream.java ${KU}/io/IOUtils.java
 
 find KTUtils -type f -name '*' -exec sed -i '' s/java\\/net\\/URLDecoder/URLDecoder/g {} +
 
