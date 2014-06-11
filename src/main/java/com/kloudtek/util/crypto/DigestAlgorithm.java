397de6a4796cbefb50f190cbf75a2f09d463e470
Yinka Fagbenro <yinkaf@gmail.com>
2014-06-11 16:48:18 +0100

    changed digest algorithm

--- src/main/java/com/kloudtek/util/crypto/DigestAlgorithm.java
+++ src/main/java/com/kloudtek/util/crypto/DigestAlgorithm.java
@@ -4,16 +4,13 @@
 
 package com.kloudtek.util.crypto;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
 
 /**
  * Enumeration of digest algorithms
  */
-@XmlEnum
 public enum DigestAlgorithm {
-    @XmlEnumValue("md5")MD5(16), @XmlEnumValue("sha1")SHA1(20, "SHA-1"), @XmlEnumValue("sha256")SHA256(32, "SHA-256"),
-    @XmlEnumValue("sha512")SHA512(64, "SHA-512");
+    MD5(16), SHA1(20, "SHA-1"), SHA256(32, "SHA-256"),
+    SHA512(64, "SHA-512");
     private int hashLen;
     private String jceId;
     private String jceHmacId;
