ca9ee439f6d2fb5df909fef61cd2057855be4e5b
Yannick Menager <ymenager@kloudtek.com>
2014-10-08 18:58:03 +0100

    Refactoring into modules

---
+++ spring/src/main/java/com/kloudtek/util/spring/SpringJSFViewScope.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014 Kloudtek Ltd
+ */
+
+package com.kloudtek.util.spring;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.web.context.request.FacesRequestAttributes;
+
+import javax.faces.context.FacesContext;
+import java.util.Map;
+
+/**
+ * Spring view scope implementation for JSF 2 view scope
+ */
+// TODO check if https://stackoverflow.com/questions/12182844/memory-leak-with-viewscoped-bean applies ?
+public class SpringJSFViewScope implements Scope {
+    public static final String CALLBACKS = "ktutilsViewScopeCallbacks";
+
+    @Override
+    public Object get(String name, ObjectFactory<?> factory) {
+        Map<String, Object> viewMap = getViewMap();
+        Object obj = viewMap.get(name);
+        if (obj == null) {
+            obj = factory.getObject();
+            viewMap.put(name, obj);
+        }
+        return obj;
+    }
+
+    private Map<String, Object> getViewMap() {
+        return FacesContext.getCurrentInstance().getViewRoot().getViewMap(true);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object remove(String name) {
+        Map<String, Object> viewMap = getViewMap();
+        Object instance = viewMap.remove(name);
+        if (instance != null) {
+            Map<String, Runnable> callbacks = (Map<String, Runnable>) viewMap.get(CALLBACKS);
+            if (callbacks != null) {
+                callbacks.remove(name);
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void registerDestructionCallback(String name, Runnable runnable) {
+        Map<String, Runnable> callbacks = (Map<String, Runnable>) getViewMap().get(CALLBACKS);
+        if (callbacks != null) {
+            callbacks.put(name, runnable);
+        }
+    }
+
+    @Override
+    public Object resolveContextualObject(String name) {
+        return new FacesRequestAttributes(FacesContext.getCurrentInstance()).resolveReference(name);
+    }
+
+    @Override
+    public String getConversationId() {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        return new FacesRequestAttributes(ctx).getSessionId() + "-" + ctx.getViewRoot().getViewId();
+    }
+}
---
+++ xml/src/test/java/com/kloudtek/util/xml/XmlUtilsTests.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2014 Kloudtek Ltd
+ */
+
+package com.kloudtek.util.xml;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Text;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import static com.kloudtek.util.xml.XPathUtils.evalXPathElement;
+import static com.kloudtek.util.xml.XPathUtils.evalXPathNode;
+import static com.kloudtek.util.xml.XmlUtils.getXPath;
+import static com.kloudtek.util.xml.XmlUtils.parse;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Test @{link XmlUtils} functionality
+ */
+public class XmlUtilsTests {
+    private static final String COW1 = "/*[name()='blob'][1]/*[name()='cow'][1]";
+    private static final String COW2 = "/*[name()='blob'][1]/*[name()='cow'][2]";
+    private static final String GOO = "/*[name()='blob'][1]/*[name()='b:goo'][2]";
+    private static final String BIRD = "/*[name()='blob'][1]/*[name()='cow'][2]/@bird";
+    private static final String TEXT1 = "/*[name()='blob'][1]/*[name()='cow'][2]/text()[1]";
+    private static final String TEXT2 = "/*[name()='blob'][1]/*[name()='cow'][2]/text()[2]";
+    private Document doc;
+
+    @BeforeClass
+    public void loadDoc() throws IOException, SAXException {
+        doc = parse(getClass().getResourceAsStream("test.xml"));
+    }
+
+    @Test
+    public void testGetXpathElement() throws IOException, SAXException, XPathExpressionException {
+        assertEquals(getXPath(evalXPathElement(COW1, doc)), COW1);
+        assertEquals(getXPath(evalXPathElement(COW2, doc)), COW2);
+    }
+
+    @Test
+    public void testGetXpathElementNS() throws IOException, SAXException, XPathExpressionException {
+        assertEquals(getXPath(evalXPathElement(GOO, doc)), GOO);
+    }
+
+    @Test
+    public void testGetXpathAttribute() throws IOException, SAXException, XPathExpressionException {
+        assertEquals(getXPath(evalXPathNode(BIRD, doc)), BIRD);
+    }
+
+    @Test
+    public void testGetXpathText() throws IOException, SAXException, XPathExpressionException {
+        final Text node1 = (Text) evalXPathNode(TEXT1, doc);
+        assertEquals(node1.getNodeValue().trim(), "river");
+        assertEquals(getXPath(node1), TEXT1);
+        final Text node2 = (Text) evalXPathNode(TEXT2, doc);
+        assertEquals(node2.getNodeValue().trim(), "cat");
+        assertEquals(getXPath(node2), TEXT2);
+    }
+
+    @Test
+    public void testXXEAttackWithoutProtection() throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
+        File tempFile = File.createTempFile("xxe", "test");
+        try {
+            FileUtils.write(tempFile, "hacked");
+            String XXE_ATTACK = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
+                    " <!DOCTYPE foo [  \n" +
+                    "  <!ELEMENT foo ANY >\n" +
+                    "  <!ENTITY xxe SYSTEM \"" + tempFile.toURI() + "\" >]><foo>&xxe;</foo>";
+            InputSource source = new InputSource(new StringReader(XXE_ATTACK));
+            Document doc = null;
+            try {
+                doc = XmlUtils.getDocumentBuilderFactory(true, true).newDocumentBuilder().parse(source);
+            } catch (SAXException e) {
+                if (e.getMessage().contains("DOCTYPE is disallowed")) {
+                    return;
+                }
+                throw e;
+            }
+            String val = XPathUtils.evalXPathString("foo/text()", doc);
+            assertTrue(!val.trim().toLowerCase().contains("hacked"));
+        } finally {
+            tempFile.delete();
+        }
+    }
+}
