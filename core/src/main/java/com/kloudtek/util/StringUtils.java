6fe2504d8cc57b8c7623525e5289e9a4c09a33d0
Yannick Menager <ymenager@kloudtek.com>
2018-01-31 18:21:24 -0800

    Added variable substitution

--- core/src/main/java/com/kloudtek/util/StringUtils.java
+++ core/src/main/java/com/kloudtek/util/StringUtils.java
@@ -4,6 +4,8 @@
 
 package com.kloudtek.util;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -118,14 +120,14 @@ public class StringUtils {
         return new Base32(0, Base64.CHUNK_SEPARATOR, useHex).decode(data);
     }
 
-    public static String urlPathEncode( String path ) {
+    public static String urlPathEncode(String path) {
         StringBuilder buffer = new StringBuilder();
         for (char c : path.toCharArray()) {
             if (UNSAFE_URLPATH.indexOf(c) >= 0) {
                 buffer.append('%');
                 buffer.append(toHex(c / 16));
                 buffer.append(toHex(c % 16));
-            } else if( c < 32 && c > 128 ) {
+            } else if (c < 32 && c > 128) {
                 buffer.append(urlEncode(Character.toString(c)));
             } else {
                 buffer.append(c);
@@ -167,75 +169,183 @@ public class StringUtils {
         }
     }
 
-    /**
-     * Substitute variables in a string.
-     * @param str String to substitute variables from
-     * @param variables Variables
-     * @return String with variables substituted
-     */
-    public static String substituteVariables( String str, Map<String,String> variables ) throws IllegalArgumentException {
-        synchronized (StringUtils.class) {
-            if( varSubFuncPattern == null ) {
-                varSubFuncPattern = Pattern.compile("([a-zA-Z]*?):(.*)");
+    public static boolean containsVariableSubstitution( String str ) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if( c == '\\' ) {
+                i++;
+            } else if( c == '$' ) {
+                if( nextChar(chars,i) == '{' ) {
+                    return true;
+                }
             }
         }
+        return false;
+    }
+
+    /**
+     * Same as calling {@link #substituteVariables(String, Map)} with neverFail set to true
+     */
+    public static String substituteVariables(String str, Map<String, String> variables) throws IllegalArgumentException {
+        return substituteVariables(str, variables, true);
+    }
+
+    /**
+     * Substitute variables in a string.
+     *
+     * @param str       String to substitute variables from
+     * @param variables Variables
+     * @param neverFail If set to true, this will suppress any errors and instead return empty string in the invalid part of the variable substituation.
+     * @return String with variables substituted
+     * @throws IllegalArgumentException If an invalid variable substituation is found and neverFail is set to false
+     */
+    public static String substituteVariables(String str, Map<String, String> variables, boolean neverFail) throws IllegalArgumentException {
         if (str == null) {
             return null;
         }
-        StringWriter result = new StringWriter();
-        StringWriter v = null;
-        VarSubState state = VarSubState.NORMAL;
-        for (char c : str.toCharArray()) {
-            switch (state) {
-                case NORMAL:
-                    if (c == '$') {
-                        state = VarSubState.STARTPARSE;
-                        v = new StringWriter();
+        char[] chars = str.toCharArray();
+        int nested = 0;
+        StringWriter newStr = new StringWriter();
+        StringWriter varStr = new StringWriter();
+        StringWriter cStr = newStr;
+        boolean varParse = false;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '\\') {
+                i++;
+                cStr.append(chars[i]);
+            } else if (c == '$') {
+                char nc = nextChar(chars, i);
+                if (nc == '{') {
+                    i++;
+                    if (!varParse) {
+                        varParse = true;
+                        cStr = varStr;
                     } else {
-                        result.append(c);
+                        nested++;
+                        varStr.append("${");
                     }
-                    break;
-                case STARTPARSE:
-                    if (c == '{') {
-                        state = VarSubState.PARSE;
-                    } else {
-                        if (c != '$') {
-                            result.append('$');
-                        }
-                        result.append(c);
-                        state = VarSubState.NORMAL;
-                    }
-                    break;
-                case PARSE:
-                    if (c == '}') {
-                        result.append(resolveVarSub(v.toString(), variables));
-                        v = null;
-                        state = VarSubState.NORMAL;
-                    } else {
-                        v.append(c);
-                    }
+                }
+            } else if (varParse && c == '}') {
+                if (nested > 0) {
+                    nested--;
+                    varStr.append('}');
+                } else {
+                    varParse = false;
+                    cStr = newStr;
+                    newStr.append(resolveVarSub(varStr.toString(), variables, neverFail));
+                    varStr = new StringWriter();
+                }
+            } else {
+                cStr.append(c);
             }
         }
-        return result.toString();
+        return newStr.toString();
     }
 
-    private static String resolveVarSub(String exp, Map<String, String> provisioningParams) throws IllegalArgumentException {
-        Matcher m = varSubFuncPattern.matcher(exp);
-        if( m.find() ) {
-            String functionName = m.group(0).toLowerCase();
-            String functionParams = m.group(1);
-            if( functionName.equals("p") ) {
+    private static char nextChar(char[] chars, int idx) {
+        int newIdx = idx + 1;
+        if (idx < chars.length) {
+            return chars[newIdx];
+        } else {
+            return 0;
+        }
+    }
 
+    private static String resolveVarSub(String exp, Map<String, String> provisioningParams, boolean neverFail) throws IllegalArgumentException {
+        if( containsVariableSubstitution(exp) ) {
+            exp = substituteVariables(exp,provisioningParams,neverFail);
+        }
+        Pattern pattern = getVarSubFuncPattern();
+        Matcher m = pattern.matcher(exp);
+        if (m.find()) {
+            String functionName = m.group(1).toLowerCase();
+            String functionParams = m.group(2);
+            boolean prefixFunc = functionName.equals("p");
+            boolean suffixFunc = functionName.equals("s");
+            if (prefixFunc || suffixFunc) {
+                String[] args = splitTwoArgFunction(functionParams, neverFail);
+                if (args != null) {
+                    String xfix = args[0];
+                    String val = resolveVarSub(args[1],provisioningParams, neverFail);
+                    if (StringUtils.isNotBlank(val)) {
+                        return suffixFunc ? xfix + val : val + xfix;
+                    } else {
+                        return val;
+                    }
+                } else {
+                    resolveVarSubFail(exp, neverFail);
+                }
+            } else if (functionName.equals("u")) {
+                return resolveVarSub(functionParams,provisioningParams, neverFail).toUpperCase();
+            } else if (functionName.equals("l")) {
+                return resolveVarSub(functionParams,provisioningParams, neverFail).toLowerCase();
+            } else if (functionName.equals("c")) {
+                return capitalize(resolveVarSub(functionParams,provisioningParams, neverFail));
+            } else if (functionName.equals("eb64")) {
+                return base64Encode(utf8(resolveVarSub(functionParams,provisioningParams, neverFail)));
+            } else if (functionName.equals("db64")) {
+                return utf8(base64Decode(resolveVarSub(functionParams,provisioningParams, neverFail)));
+            } else if (functionName.equals("t")) {
+                return functionParams;
             } else {
-                throw new IllegalArgumentException("Invalid variable substitution variable name: "+functionName+" in variable substitution string "+functionName);
+                resolveVarSubFail(exp, neverFail);
             }
         } else {
             String val = provisioningParams.get(exp);
-            return val != null ? val : "";
+            if (val == null) {
+                if (!neverFail) {
+                    throw new IllegalArgumentException("Variable not found: " + exp);
+                }
+            } else {
+                return val;
+            }
+        }
+        return "";
+    }
+
+    private static String resolveVarSubFail(String exp, boolean neverFail) {
+        if (!neverFail) {
+            throw new IllegalArgumentException("Invalid variable substitution expression: " + exp);
+        } else {
+            return "";
         }
     }
 
-    public enum VarSubState {
-        NORMAL, STARTPARSE, PARSE
+    private static synchronized Pattern getVarSubFuncPattern() {
+        if (varSubFuncPattern == null) {
+            varSubFuncPattern = Pattern.compile("([a-zA-Z]*?):(.*)");
+        }
+        return varSubFuncPattern;
+    }
+
+    @Nullable
+    private static String[] splitTwoArgFunction(String str, boolean neverFail) {
+        StringWriter arg1 = new StringWriter();
+        StringWriter arg2 = new StringWriter();
+        boolean potentialMatch = false;
+        boolean match = false;
+        for (char c : str.toCharArray()) {
+            if (match) {
+                arg2.append(c);
+            } else if (c == ':') {
+                if (potentialMatch) {
+                    arg1.append(':');
+                }
+                potentialMatch = !potentialMatch;
+            } else if (potentialMatch) {
+                match = true;
+                arg2.append(c);
+            } else {
+                arg1.append(c);
+            }
+        }
+        if (match) {
+            return new String[]{arg1.toString(), arg2.toString()};
+        } else {
+            resolveVarSubFail(str, neverFail);
+            return null;
+        }
     }
 }
--- core/src/test/java/com/kloudtek/util/StringUtilsTest.java
+++ core/src/test/java/com/kloudtek/util/StringUtilsTest.java
@@ -3,6 +3,7 @@ package com.kloudtek.util;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.jetbrains.annotations.NotNull;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.URL;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -28,6 +30,86 @@ public class StringUtilsTest {
     public void testUrlPathEncode() throws Exception {
         String path = decodeUrl("/test/"+StringUtils.urlPathEncode(PATH));
         Assert.assertEquals(path,"/test/"+PATH);
+    }
+
+    @Test
+    public void testVarSubSimple() {
+        assertEquals("blabarbleh",StringUtils.substituteVariables("bla${foo}bleh", genVars()));
+    }
+
+    @Test
+    public void testVarSubEscape() {
+        assertEquals("bla${foo}bleh",StringUtils.substituteVariables("bla\\${foo}bleh", genVars()));
+    }
+
+    @Test
+    public void testVarSubEscape2() {
+        assertEquals("bla$bleh",StringUtils.substituteVariables("bla\\$bleh", genVars()));
+    }
+
+    @Test
+    public void testVarSubNeverFail() {
+        assertEquals("blableh",StringUtils.substituteVariables("bla${xxx}bleh", genVars()));
+    }
+
+    @Test
+    public void testVarSubNeverFailBadArg() {
+        assertEquals("blableh",StringUtils.substituteVariables("bla${s:fsd}bleh", genVars()));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testVarSubFail() {
+        StringUtils.substituteVariables("bla${xxx}bleh", genVars(),false);
+    }
+
+    @Test
+    public void testVarSubSuffix() {
+        assertEquals("bla-bar",StringUtils.substituteVariables("bla${s:-:foo}", genVars()));
+    }
+
+    @Test
+    public void testVarSubSuffixComma() {
+        assertEquals("bla:bar",StringUtils.substituteVariables("bla${s::::foo}", genVars()));
+    }
+
+    @Test
+    public void testVarSubPrefix() {
+        assertEquals("bar-bla",StringUtils.substituteVariables("${p:-:foo}bla", genVars()));
+    }
+
+    @Test
+    public void testVarSubPrefixComman() {
+        assertEquals("bar:bla",StringUtils.substituteVariables("${p::::foo}bla", genVars()));
+    }
+
+    @Test
+    public void testVarSubSuffixEmpty() {
+        assertEquals("bla",StringUtils.substituteVariables("bla${s:-:xxx}", genVars()));
+    }
+
+    @Test
+    public void testVarSubUpperCase() {
+        assertEquals("blaBAR",StringUtils.substituteVariables("bla${u:foo}", genVars()));
+    }
+
+    @Test
+    public void testVarSubLowerCase() {
+        assertEquals("blamoo",StringUtils.substituteVariables("bla${l:boo}", genVars()));
+    }
+
+    @Test
+    public void testVarCapitalize() {
+        assertEquals("blaBar",StringUtils.substituteVariables("bla${c:foo}", genVars()));
+    }
+
+    @Test
+    public void testVarString() {
+        assertEquals("blaBl:~ a",StringUtils.substituteVariables("bla${t:Bl:~ a}", genVars()));
+    }
+
+    @Test
+    public void testVarInVar() {
+        assertEquals("blabarbleh",StringUtils.substituteVariables("bla${${alias}}bleh", genVars()));
     }
 
     public String decodeUrl(final String path) throws Exception {
@@ -45,7 +127,7 @@ public class StringUtilsTest {
             public void run() {
                 try {
                     ThreadUtils.sleep(500);
-                    InputStream is = new URL("http://localhost:" + port + path).openStream();
+                    new URL("http://localhost:" + port + path).openStream();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -55,6 +137,15 @@ public class StringUtilsTest {
             wait();
         }
         return this.decodedPath;
+    }
+
+    @NotNull
+    private HashMap<String, String> genVars() {
+        HashMap<String,String> vars = new HashMap<String, String>();
+        vars.put("foo","bar");
+        vars.put("boo","MOO");
+        vars.put("alias","foo");
+        return vars;
     }
 
     public class DecodeServlet extends HttpServlet {
